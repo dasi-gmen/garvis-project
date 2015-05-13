a2025c932ef29771be1ed38469c3da6607576b84
juliocat <juliocat@outlook.com>
2015-05-13 18:09:51 +0200

    Grabar programa

---
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoGarvisCognitivo/objetivos/GrabarProgramaTV.java
@@ -0,0 +1,10 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionDialogoGarvisCognitivo.objetivos;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+
+public class GrabarProgramaTV extends Objetivo {
+	   /** Crea una nueva instancia de DarAcceso */
+	  public GrabarProgramaTV() {
+	       super.setgoalId("GrabarPrograma");
+	  }
+
+}
