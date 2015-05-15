b177e6cb0874828c431599dd756dc7ce785ff1da
ochamache <oswear@gmail.com>
2015-05-15 22:56:14 +0200

    Biblioteca
    Obtener Lista de Libros -fase 1

---
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoGarvisCognitivo/tareas/ObtenerLibros.java
@@ -0,0 +1,67 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionDialogoGarvisCognitivo.tareas;
+
+
+import icaro.aplicaciones.agentes.AgenteAplicacionDialogoGarvisCognitivo.objetivos.ObtenerInfoInterlocutor;
+import icaro.aplicaciones.informacion.gestionGarvis.VocabularioGeneralGarvis;
+import icaro.aplicaciones.recursos.comuGarvisChat.ItfUsoComuGarvisChat;
+import icaro.infraestructura.entidadesBasicas.NombresPredefinidos;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.CausaTerminacionTarea;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Focus;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+import icaro.infraestructura.recursosOrganizacion.recursoTrazas.imp.componentes.InfoTraza;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class ObtenerLibros extends TareaSincrona{
+
+  /**
+   *  Constructor
+   *
+   *@param           Description of the Parameter
+   *@param    Description of the Parameter
+   */
+    private Objetivo contextoEjecucionTarea = null;
+  @Override
+	public void ejecutar(Object... params) {
+  /**
+   * Produce un saludo inicial y una presentacion de funcionalidad inicial al entrar en el sistema
+   */
+   String identDeEstaTarea=this.getIdentTarea();
+            String identAgenteOrdenante = this.getIdentAgente();
+          String identInterlocutor = (String)params[0];
+                    try {
+//         // Se busca la interfaz del recurso en el repositorio de interfaces 
+		ItfUsoComuGarvisChat recComunicacionChat = (ItfUsoComuGarvisChat) NombresPredefinidos.REPOSITORIO_INTERFACES_OBJ.obtenerInterfazUso(
+						VocabularioGeneralGarvis.IdentRecursoComunicacionChat);          
+                if (recComunicacionChat!=null){
+                    recComunicacionChat.comenzar(identAgenteOrdenante);
+                    List<String> supplierNames = new ArrayList<String>();
+                    supplierNames.add("sup1");
+                    supplierNames.add("sup2");
+                    supplierNames.add("sup3");
+                    String mensaje1 = VocabularioGeneralGarvis.HasDichoBiblioteca2;
+                    String mensajeAenviar = VocabularioGeneralGarvis.HasDichoBiblioteca2;
+                    Iterator<String> iterator = supplierNames.iterator();
+                    while (iterator.hasNext()) {
+                          //System.out.println(iterator.next());
+                          mensajeAenviar +=iterator.next();
+                        } 
+                    recComunicacionChat.enviarMensagePrivado(mensajeAenviar);
+                }
+                else {
+                    identAgenteOrdenante = this.getAgente().getIdentAgente();
+                     this.generarInformeConCausaTerminacion(identDeEstaTarea, contextoEjecucionTarea, identAgenteOrdenante, "Error-AlObtener:Interfaz:"+
+                             VocabularioGeneralGarvis.IdentRecursoComunicacionChat, CausaTerminacionTarea.ERROR);
+                        }
+                    } catch(Exception e) {
+                        this.generarInformeConCausaTerminacion(identDeEstaTarea, contextoEjecucionTarea, identAgenteOrdenante, "Error-Acceso:Interfaz:"+
+                                VocabularioGeneralGarvis.IdentRecursoComunicacionChat, CausaTerminacionTarea.ERROR);
+			e.printStackTrace();
+		}
+	}
+  
+
+}
