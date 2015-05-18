504b9b2b13db106393354c28960b91d44af97b5e
ochamache <oswear@gmail.com>
2015-05-18 17:22:28 +0200

    Biblioteca fase 4
    Biblioteca fase 4

---
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoGarvisCognitivo/objetivos/IdentificarLibro.java
@@ -0,0 +1,12 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionDialogoGarvisCognitivo.objetivos;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+
+public class IdentificarLibro extends Objetivo{
+
+		public IdentificarLibro() {
+			super.setgoalId("IdentificarLibro");
+		}
+		
+	
+
+}
