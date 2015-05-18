9d2e2d66269bd4db8a2fc2947960d9564961f56c
Vitucho2015 <vmarisca@ucm.es>
2015-05-18 19:06:01 +0200

    CAMBIO DE RESPUESTA A HOLA
    RANDON DE RESPUESTA EN SALUDO

--- src/icaro/aplicaciones/informacion/gestionGarvis/VocabularioGeneralGarvis.java
+++ src/icaro/aplicaciones/informacion/gestionGarvis/VocabularioGeneralGarvis.java
@@ -20,12 +20,12 @@ public class VocabularioGeneralGarvis {
     public static final String IdentRecursoPersistenciaChat= "PersistenciaChat1";
     public static final String InfoUsuarioYaExiste= "infoUsuarioYaExistente";
     public static final String InfoUsuarioAltaGuardada="InfoAltaUsuarioGuardada";
-    public static final String SaludoInicial1="Hola soy GARVIS y estoy aquí para ayudarte a controlar tu casa y tus electrodomésticos";
-    public static final String SaludoInicial2="Hola ";
-    public static final String PeticionInformacionGeneral1="Dime que deseas";
+    public static final String SaludoInicial1="Hola soy GARVIS y estoy aquí para ayudarte a controlar tu casa y tus electrodomesticos";
+    public static final String SaludoInicial2="Hola :Queonda :Que hay de nuevo";
+    public static final String PeticionInformacionGeneral1="Dime que deseas :Dime te escucho :Estoy para atenderte :Estoy a tus ordenes";
     public static final String RespuestaNoEniendo1="Lo siento pero no entiendo nada de lo que dices";
     public static final String RespuestaNoEniendo2="Lo siento pero sigo sin entender nada";
-    public static final String RespuestaNoEniendo3="definitivamente no me entero de lo que dice , creo que vamos a terminar la conversación";
+    public static final String RespuestaNoEniendo3="definitivamente no me entero de lo que dice , creo que vamos a terminar la conversacion";
     public static final String Despedida1="Bueno tenemos que dejarlo ha sido un placer";
     public static final String NombreTipoNoticacionSaludo="saludo";
     public static final String NombreTipoNoticacionGarvis="garvis";
